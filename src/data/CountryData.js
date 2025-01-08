e2aaf5938e294156a7d7737d271e5224436c4851
Ahmad Anees <83137173+AneeVerse@users.noreply.github.com>
2025-01-08 18:16:13 +0530

    us,uk ame change

--- src/data/CountryData.js
+++ src/data/CountryData.js
@@ -2083,7 +2083,7 @@ const CountryData = {
   ],
   Europe: [
     {
-      name: "United Kingdom",
+      name: "United Kingdom - (UK)",
       flag: "/images/flags/uk.webp",
       landmark: "/images/landmarks/Big Ben tourist places in United Kingdom UK Visa.webp",
       landmarkName: "Big Ben",
@@ -3902,7 +3902,7 @@ const CountryData = {
   ],
   NorthAmerica: [
     {
-      name: "United States",
+      name: "United States - (USA)",
       flag: "/images/flags/us.webp",
       landmark: "/images/landmarks/Tourist Places in United States Visa.webp",
       landmarkName: "Statue of Liberty",
